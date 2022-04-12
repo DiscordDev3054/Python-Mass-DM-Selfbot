5bf6bb67594b4c33357bbd25a461a1b96471d5ea
DiscordDev3054 <103537793+DiscordDev3054@users.noreply.github.com>
2022-04-12 16:47:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 import os
 os.system("pip install requests")
-os.system("python -m pip install --user --upgrade git+https://github.com/DiscordDev3054/Discord-S.C.U.M.git#egg=discum")
+os.system("pip install discum")
