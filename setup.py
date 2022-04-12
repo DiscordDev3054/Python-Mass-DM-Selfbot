c57c9107add1ece3a9f8066532cf9264daf30a13
DiscordDev3054 <103537793+DiscordDev3054@users.noreply.github.com>
2022-04-12 17:05:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 import time
+print("When asked to uninstall the modules, type 'y' both times. This installs custom versions of the modules so it needs to rienstall them.")
 os.system("pip uninstall discum")
 os.system("pip uninstall requests")
 os.system("pip install requests")
